67f9d7c0a2e6f505f3756794faf1aec5bed3ae6b
Luiza Fernandes <luhqfernandes@gmail.com>
2021-04-25 16:44:29 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+function montar(){
+
+    let numLinhas = parseFloat(document.getElementById("num1").value);
+    let numColunas = parseFloat(document.getElementById("num2").value);
+
+    document.getElementById("mxn").textContent = `Matriz = ${numLinhas} X ${numColunas} `
+
+    for(var i = 1; i <= numLinhas; i++){
+        for(var j = 1; j <= numColunas; j++){
+            document.getElementById("input-array").innerHTML += `<input type="text" id="array[${i}][${j}]"/>`
+        }
+        document.getElementById("input-array").innerHTML += `<br>`
+    }
+    document.getElementById("input-array").innerHTML += '<button onClick="buscar()"> Enviar </button><br><br>'
+}
+
+function buscar(){
+
+    let numLinhas = parseFloat(document.getElementById("num1").value);
+    let numColunas = parseFloat(document.getElementById("num2").value);
+  
+    for(var i = 1; i <= numLinhas; i++){
+
+        for(var j = 1; j <= numColunas; j++){
+            
+            let valor;
+
+            if(j == numColunas){
+                valor = `${document.getElementById(`array[${i}][${j}]`).value}`;
+                document.getElementById("array").innerHTML += `${valor}<br>`
+            }
+            else{
+                valor = `${document.getElementById(`array[${i}][${j}]`).value}`;
+            document.getElementById("array").innerHTML += `${valor}, `
+
+            }
+            console.log(` Matriz[${i}][${j}] = ${valor}`); 
+        } 
+    }
+}
