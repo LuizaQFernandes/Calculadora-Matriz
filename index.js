524d1b284820e846eafbbae3278bf7bfc71987cc
Luiza Fernandes <luhqfernandes@gmail.com>
2021-05-02 14:10:17 -0300

    matriz transposta added

--- index.js
+++ index.js
@@ -1,25 +1,61 @@
-function montar(){
 
-    let numLinhas = parseFloat(document.getElementById("num1").value);
-    let numColunas = parseFloat(document.getElementById("num2").value);
+//MATRIZ TRANSPOSTA
 
-    document.getElementById("mxn").textContent = `Matriz = ${numLinhas} X ${numColunas} | Insira os valores da matriz `
+function InícioMatrizTransposta(){      //NÚMERO DE LINHAS E COLUNAS DA MATRIZ 
+
+    document.getElementById("escolha").innerHTML += `<br>
+
+    <center><h2>Insira a ordem da Matriz</h2></center>
+    
+    <div class="container">
+        <div>
+            Digite o número de linhas <input type="number"  id="linhas" /><br>
+            Digite o número de colunas <input type="number"  id="colunas" /><br> 
+        </div>
+    </div>
+
+    <div class="container">
+        <button class="buttonEnviar" onClick="montarMatrizTransposta()"> Enviar </button>
+    </div>
+
+    <div id="título-input"></div> 
+
+    <div class="container">
+        <div id="input-array"></div>
+    </div>
+
+    <div id="títuloMatrizTransposta"></div>
+
+    <div class="container">
+        <div id="MatrizT"></div>
+    </div>
+
+    <div id="buttonVoltar"></div>
+`
+}
+
+function montarMatrizTransposta(){   //MONTA O INPUT  
+
+    let numLinhas = parseFloat(document.getElementById("linhas").value);
+    let numColunas = parseFloat(document.getElementById("colunas").value);
+
+    document.getElementById("título-input").innerHTML = `<center><h2>Insira os valores da matriz </h2></center>`
 
     for(var i = 1; i <= numLinhas; i++){
         for(var j = 1; j <= numColunas; j++){
-            document.getElementById("input-array").innerHTML += `<input type="text" id="array[${i}][${j}]"/>`
+            document.getElementById("input-array").innerHTML += `<input type="number" id="array[${i}][${j}]"/>`
         }
         document.getElementById("input-array").innerHTML += `<br>`
     }
-    document.getElementById("input-array").innerHTML += '<div class="container"><button type="button" class="button" onClick="buscar()" >Enviar</button> </div>'
+    document.getElementById("input-array").innerHTML += '<div class="container"><button type="button" class="buttonEnviar" onClick="resultadoMatrizTransposta()" >Enviar</button> </div>'
 }
 
-function buscar(){
+function resultadoMatrizTransposta(){ //MONTA O MATRIZ TRANSPOSTA  
 
-    let numLinhas = parseFloat(document.getElementById("num1").value);
-    let numColunas = parseFloat(document.getElementById("num2").value);
+    let numLinhas = parseFloat(document.getElementById("linhas").value);
+    let numColunas = parseFloat(document.getElementById("colunas").value);
   
-    for(var i = 1; i <= numLinhas; i++){
+   /*for(var i = 1; i <= numLinhas; i++){
 
         for(var j = 1; j <= numColunas; j++){
             
@@ -27,14 +63,51 @@ function buscar(){
 
             if(j == numColunas){
                 valor = `${document.getElementById(`array[${i}][${j}]`).value}`;
-                document.getElementById("array").innerHTML += `${valor}<br>`
+                //document.getElementById("array").innerHTML += `${valor}<br>`
             }
             else{
                 valor = `${document.getElementById(`array[${i}][${j}]`).value}`;
-                document.getElementById("array").innerHTML += `${valor}, `
+                //document.getElementById("array").innerHTML += `${valor}, `
 
             }
             console.log(` Matriz[${i}][${j}] = ${valor}`); 
         } 
-    }
+    }*/
+
+    console.log(` Matriz Transposta`); 
+
+    document.getElementById("títuloMatrizTransposta").innerHTML += `<br><center><h2>Matriz Transposta</h2></center>`
+
+    var aux = numLinhas
+    numLinhas = numColunas;
+    numColunas = aux;
+
+    for(var i = 1; i <= numLinhas; i++){
+
+        for(var j = 1; j <= numColunas; j++){
+            
+            let valor;
+
+            if(j == numColunas){
+                valor = `${document.getElementById(`array[${j}][${i}]`).value}`;
+                document.getElementById("MatrizT").innerHTML += `<input type="number" id="arrayT[${j}][${i}]" readOnly placeholder="${valor}"/> <br>`
+            }
+            else{
+                valor = `${document.getElementById(`array[${j}][${i}]`).value}`;
+                document.getElementById("MatrizT").innerHTML += `<input type="number" id="arrayT[${j}][${i}]" readOnly placeholder="${valor}"/>`
+
+            }
+            console.log(` MatrizT[${j}][${i}] = ${valor}`); 
+        } 
+    } 
+
+    document.getElementById("buttonVoltar").innerHTML += '<br><br><center><button type="button" class="buttonVoltar" onClick="Voltar()" >Voltar ao Menu</button> </center>'
+
 }
+
+function Voltar(){
+    document.getElementById("escolha").innerHTML = ``
+}
+
+
+
